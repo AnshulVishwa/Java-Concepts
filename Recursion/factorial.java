a4195deff08c2e24faf0624381e09c785721baef
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-05-11 10:54:37 +0530

    Update

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -1,13 +1,13 @@
 public class factorial {
-    public static void factorial( int n , int init , int sum ){
+    public static void factoria( int n , int init , int sum ){
         if ( n == 0 ){
             System.out.println(sum);
             return;
         }
         sum *= init  ;
-        factorial( n-1 , init+1 , sum );
+        factoria( n-1 , init+1 , sum );
     }
     public static void main( String argd[] ){
-        factorial( 5 , 1 , 1 );
+        factoria( 5 , 1 , 1 );
     }
 }
